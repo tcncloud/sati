33ed832522516f3a59a7f4c29338b51871efed6b
Florin Stan <597933+namtzigla@users.noreply.github.com>
2025-04-17 09:57:51 -0600

    fix: change call_sid type from Long to String in ConnectedParty and DialResponse models for consistency

--- core/src/main/java/com/tcn/exile/models/ConnectedParty.java
+++ core/src/main/java/com/tcn/exile/models/ConnectedParty.java
@@ -21,7 +21,7 @@ import io.micronaut.serde.annotation.Serdeable;
 
 @Serdeable
 public record ConnectedParty(
-    @JsonProperty("call_sid") long callSid,
+    @JsonProperty("call_sid") String callSid,
     @JsonProperty("call_type") CallType callType,
     @JsonProperty("inbound") boolean inbound
 ) {
