193b2e8f119425abdf1d9fb1e9c1e17c7e7bc5a9
morpheus <service_account_f92c3a3a90cccc710a8c855615377eae@noreply.git.tcncloud.net>
2026-05-20 16:16:55 +0000

    fix: Format ScrubListDto.java to satisfy Spotless

--- web/src/main/java/com/tcn/exile/web/dto/ScrubListDto.java
+++ web/src/main/java/com/tcn/exile/web/dto/ScrubListDto.java
@@ -2,4 +2,5 @@ package com.tcn.exile.web.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record ScrubListDto(String scrubListId, boolean readOnly, @JsonProperty("contentType") ScrubListType scrubType) {}
+public record ScrubListDto(
+    String scrubListId, boolean readOnly, @JsonProperty("contentType") ScrubListType scrubType) {}
